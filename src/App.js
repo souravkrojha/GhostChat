08c66b72edde4a56cc23ff3527dd0e0fee4af585
Sourav Ojha <sojha7724@gmail.com>
2020-09-27 10:18:17 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,157 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import "./App.css";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import { FiSend, FiGithub, FiMessageCircle } from "react-icons/fi";
+import { FaReact } from "react-icons/fa";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBV292g0fp3vUNhO2CY0weLlLPjWB-z4_Q",
+  authDomain: "ghostchat-6e88e.firebaseapp.com",
+  databaseURL: "https://ghostchat-6e88e.firebaseio.com",
+  projectId: "ghostchat-6e88e",
+  storageBucket: "ghostchat-6e88e.appspot.com",
+  messagingSenderId: "181164989959",
+  appId: "1:181164989959:web:ec3a1360e94a655ee9d4a2",
+  measurementId: "G-9Z9KF9F4K4",
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
+  const [user] = useAuthState(auth);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>
+          <a href={"https://reactjs.org/"} rel="noopener">
+            <FaReact />
+          </a>
+          &nbsp;
+          <a href={"https://github.com/bughunter-99"} rel="noopener">
+            <FiGithub />
+          </a>
+          &nbsp;
+          <FiMessageCircle />
+          &nbsp;
+        </h1>
+        <SignOut />
       </header>
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
+    </div>
+  );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+
+  return (
+    <div>
+      <button
+        type="button"
+        className="google-button"
+        onClick={signInWithGoogle}
+      >
+        <span className="google-button__icon">
+          <svg viewBox="0 0 366 372" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M125.9 10.2c40.2-13.9 85.3-13.6 125.3 1.1 22.2 8.2 42.5 21 59.9 37.1-5.8 6.3-12.1 12.2-18.1 18.3l-34.2 34.2c-11.3-10.8-25.1-19-40.1-23.6-17.6-5.3-36.6-6.1-54.6-2.2-21 4.5-40.5 15.5-55.6 30.9-12.2 12.3-21.4 27.5-27 43.9-20.3-15.8-40.6-31.5-61-47.3 21.5-43 60.1-76.9 105.4-92.4z"
+              id="Shape"
+              fill="#EA4335"
+            />
+            <path
+              d="M20.6 102.4c20.3 15.8 40.6 31.5 61 47.3-8 23.3-8 49.2 0 72.4-20.3 15.8-40.6 31.6-60.9 47.3C1.9 232.7-3.8 189.6 4.4 149.2c3.3-16.2 8.7-32 16.2-46.8z"
+              id="Shape"
+              fill="#FBBC05"
+            />
+            <path
+              d="M361.7 151.1c5.8 32.7 4.5 66.8-4.7 98.8-8.5 29.3-24.6 56.5-47.1 77.2l-59.1-45.9c19.5-13.1 33.3-34.3 37.2-57.5H186.6c.1-24.2.1-48.4.1-72.6h175z"
+              id="Shape"
+              fill="#4285F4"
+            />
+            <path
+              d="M81.4 222.2c7.8 22.9 22.8 43.2 42.6 57.1 12.4 8.7 26.6 14.9 41.4 17.9 14.6 3 29.7 2.6 44.4.1 14.6-2.6 28.7-7.9 41-16.2l59.1 45.9c-21.3 19.7-48 33.1-76.2 39.6-31.2 7.1-64.2 7.3-95.2-1-24.6-6.5-47.7-18.2-67.6-34.1-20.9-16.6-38.3-38-50.4-62 20.3-15.7 40.6-31.5 60.9-47.3z"
+              fill="#34A853"
+            />
+          </svg>
+        </span>
+        <span className="google-button__text">Sign in with Google</span>
+      </button>
+    </div>
+  );
+}
+
+function SignOut() {
+  return (
+    auth.currentUser && (
+      <button className="sign-out" onClick={() => auth.signOut()}>
+        SignOut
+      </button>
+    )
+  );
+}
+
+function ChatRoom() {
+  const messageRef = firestore.collection("messages");
+  const query = messageRef.orderBy("createdAt").limit(25);
+  const [messages] = useCollectionData(query, { idField: "id" });
+  const [formValue, setFormValue] = useState("");
+  const dummy = useRef();
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messageRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL,
+    });
+    setFormValue("");
+    dummy.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <React.Fragment>
+      <main>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+        <div ref={dummy}></div>
+      </main>
+      <form onSubmit={sendMessage}>
+        <input
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+        />
+        <button type="submit">
+          <FiSend style={{ fontSize: 26 }} />
+        </button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
+  return (
+    <div className={`message ${messageClass}`}>
+      <img
+        src={
+          photoURL || "https://api.adorable.io/avatars/23/abott@adorable.png"
+        }
+        alt="profile"
+      />
+      <p>{text}</p>
     </div>
   );
 }
